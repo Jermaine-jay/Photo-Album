73cf7c58708efa2f920fc7fcac7308e3e81e18ba
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2023-08-23 10:32:34 +0100

    Removed unnecessary Namespaces.

--- ImageApp.DAL/Entities/Picture.cs
+++ ImageApp.DAL/Entities/Picture.cs
@@ -1,12 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ImageApp.DAL.Entities
 {
     public class Picture
     {
         [Key]
-        public string? Id { get; set; }= Guid.NewGuid().ToString();
+        public string? Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
